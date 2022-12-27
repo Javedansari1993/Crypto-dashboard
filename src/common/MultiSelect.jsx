f70d68c9e1edff0a28a0b616071cb8f2861abd98
Javed <javed.cabm@gmail.com>
2022-12-27 13:45:10 +0530

    dropdown fix

--- src/common/MultiSelect.jsx
+++ src/common/MultiSelect.jsx
@@ -25,7 +25,7 @@ export default function MultiSelect({ options , onItemClick, values}) {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="absolute left-0 md:left-auto md:right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="p-1 h-80 overflow-y-auto">
                         {
                             options.sort((a,b) => {return values.includes(a.id) ? -1 : 1 }).map((el, i) => (
